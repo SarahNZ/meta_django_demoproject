31e9e502b03ef9583d1497143a180d39f33c62a0
Sarah Stenfert <sarah.stenfert@gmail.com>
2025-07-16 09:33:42 +1200

    Completed the models & migrations practical exercises

--- demoapp/models.py
+++ demoapp/models.py
@@ -1,7 +1,5 @@
-from unicodedata import name
 from django.db import models
 
-# Create your models here.
 class Menu(models.Model):
     name = models.CharField(max_length = 100)
     cuisine = models.CharField(max_length = 100)
@@ -9,3 +7,19 @@ class Menu(models.Model):
 
     def __str__(self):
         return f"{self.name} : {self.cuisine}"
+
+class Customer(models.Model):
+    name = models.CharField(max_length = 255)
+    reservation_day = models.CharField(max_length = 20, default = 'Monday')
+    seats = models.IntegerField(default = 2)
+
+    def __str__(self):
+        return self.name
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length = 255)
+    customer = models.ForeignKey(
+        Customer, 
+        on_delete = models.CASCADE,
+        related_name = 'Vehicle'
+    )
