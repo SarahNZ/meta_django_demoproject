20f6745032c4652cf030eec2bee2098b13a5a768
Sarah Stenfert <sarah.stenfert@gmail.com>
2025-07-21 19:15:21 +1200

    Changes to the admin.py and models as part of learning how to use the Django admin site

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
-from .models import Input
+from .models import Input, Person, Reservation
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
 
 # Register your models here.
 admin.site.register(Input)
+admin.site.register(Reservation)
+# admin.site.register(Person)
+
+# Unregister the provided model admin
+admin.site.unregister(User)
+
+# Register own admin user
+@admin.register(User)
+class NewAdmin(UserAdmin):
+    add_form = UserCreationForm  # Fixes the 'NoneType' error
+    readonly_fields = ['date_joined',]
+
+    # Explicitly define add_fieldsets to avoid relying on anything else
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+    def get_form(self, request, obj = None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['username'].disabled = True
+
+        return form # Make sure to return the form
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name")
+    search_fields = ("first_name__startswith", ) # comma is because must be tuple
--- myapp/models.py
+++ myapp/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 
-# Implementation using ModelForm class
-
 class Input(models.Model):
     first_name = models.CharField(max_length = 200)
     last_name = models.CharField(max_length = 200)
     time_log = models.TimeField(help_text = "Enter the exact time")
 
-# class Person(models.Model):
-#     person_name = models.CharField(max_length=20)
-#     email = models.EmailField()
-#     phone = models.CharField(max_length=20)
+class Person(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
+    # person_name = models.CharField(max_length=20)
+    # email = models.EmailField()
+    # phone = models.CharField(max_length=20)
+
+class Reservation(models.Model):
+    name = models.CharField(max_length = 100, blank = True)
+    contact = models.CharField('Phone number', max_length = 100)
+    time = models.TimeField()
+    count = models.IntegerField()
+    notes = models.CharField(max_length = 300, blank = True)
+
+    def __str__(self):
+        return self.name
